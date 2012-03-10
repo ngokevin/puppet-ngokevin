072ac0f18356ded116bbef00026fb92b6497633e
Kevin Ngo <ktngo09@gmail.com>
2012-03-10 03:46:38 +0000

    git email/name/color configs, tzdata puppet class

--- manifests/classes/git.pp
+++ manifests/classes/git.pp
@@ -4,4 +4,48 @@ class git {
         ensure => installed;
     }
 
+    exec { "git_name":
+        command => "git config --global User.name '$NAME'",
+        require => Package["git"],
+        cwd => "$USER_DIR",
+        user => "$USER";
+    }
+
+    exec { "git_email":
+        command => "git config --global User.email '$EMAIL'",
+        require => Package["git"],
+        cwd => "$USER_DIR",
+        user => "$USER";
+    }
+
+    exec { "colors":
+        command => "git config color.ui true",
+        require => Package["git"],
+        returns => [0, 255],
+        cwd => "$USER_DIR",
+        user => "$USER";
+    }
+
+    exec { "root_git_name":
+        command => "git config --global User.name '$NAME'",
+        require => Package["git"],
+        cwd => "/",
+        user => root;
+    }
+
+    exec { "root_git_email":
+        command => "git config --global User.email '$EMAIL'",
+        require => Package["git"],
+        cwd => "/",
+        user => root;
+    }
+
+    exec { "root_colors":
+        command => "git config color.ui true",
+        require => Package["git"],
+        returns => [0, 255],
+        cwd => "/",
+        user => root;
+    }
+
 }
--- manifests/puppet-ngokevin.pp
+++ manifests/puppet-ngokevin.pp
@@ -4,6 +4,9 @@ $USER = "ngoke"
 $USER_DIR = "/home/ngoke"
 $PUPPET_DIR = "/home/ngoke/Code/puppet-ngokevin"
 
+$NAME = "Kevin Ngo"
+$EMAIL = "ktngo09@gmail.com"
+
 Exec {
     path => "/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin",
 }
@@ -16,6 +19,7 @@ class puppet-ngokevin {
         vim: require => Class[init];
         git: require => Class[init];
         python: require => Class[init];
+        tzdata: require => Class[init];
 
         wok: require => [Class[git], Class[python]];
         ngokevin: require => Class[wok];
