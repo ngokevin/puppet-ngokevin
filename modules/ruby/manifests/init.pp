0e1c37d1544b2ad475e28a5aaee5ef67557a60ff
Kevin Ngo <ktngo09@gmail.com>
2012-05-02 20:13:46 -0700

    use ruby1.9.1

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -1,33 +1,9 @@
 class ruby {
-    package { "ruby":
+    package { "ruby1.9.1":
         ensure => installed;
     }
 
-    package { "ruby-dev":
+    package { "ruby1.9.1-dev":
         ensure => installed;
-    }
-
-    exec { "rubygems_wget":
-        command => "wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz",
-        cwd => "/tmp",
-        user => "$USER";
-    }
-
-    exec { "rubygems_extract":
-        command => "tar -xzf rubygems-1.8.24.tgz",
-        cwd => "/tmp",
-        require => [
-            Exec["rubygems_wget"],
-        ],
-        user => root;
-    }
-
-    exec {"rubygems_install":
-        command => "ruby setup.rb",
-        cwd => "/tmp/rubygems-1.8.24",
-        require => [
-            Exec["rubygems_install"],
-        ],
-        user => root;
     }
 }
