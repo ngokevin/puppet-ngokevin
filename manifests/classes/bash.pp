d800ac5f89200eb5d4f9d2ab2ab1e276a12956c8
Kevin Ngo <ktngo09@gmail.com>
2012-12-19 22:40:59 -0800

    bashalias

--- manifests/classes/bash.pp
+++ manifests/classes/bash.pp
@@ -1,6 +1,7 @@
 class bash {
 
     $BASHRC = "$PUPPET_DIR/files/home/bashrc"
+    $BASHALIASES = "$PUPPET_DIR/files/home/bashaliases"
 
     file { "root_bashrc":
         path => "/root/.bashrc",
@@ -11,6 +12,18 @@ class bash {
     file { "user_bashrc":
         path => "$USER_DIR/.bashrc",
         source => "$BASHRC",
+        owner => "$USER", group => "$USER", mode => 644;
+    }
+
+    file { "root_bashaliases":
+        path => "/root/.bash_aliases",
+        source => "$BASHALIASES",
+        owner => root, group => root, mode => 644;
+    }
+
+    file { "user_bashaliases":
+        path => "$USER_DIR/.bash_aliases",
+        source => "$BASHRCALIASES",
         owner => "$USER", group => "$USER", mode => 644;
     }
 
